e0ad42a9d907980c33e35375bef7aa0afdd13f5e
Nashe Dan <tinashydaniel@gmail.com>
2024-05-03 23:10:23 +0200

    create todo

--- public/app.js
+++ public/app.js
@@ -4,14 +4,38 @@ $(document).ready(function() {
   .catch(function(err) {
     throw Error (err);
   });
+
+  $('#todoInput').keypress(function(event) {
+    if (event.which == 13) {
+      createTodo();
+    }
+  });
 });
 
 function addTodos(todos) {
   todos.forEach(function(todo) {
-    var newTodo = $('<li class="task">'+todo.name +'</li>');
-    if (todo.completed) {
-      newTodo.addClass("done");
-    }
-    $('.list').append(newTodo);
+    addTodo(todo);
   });
+}
+
+function addTodo(todo) {
+  var newTodo = $('<li class="task">'+todo.name +'</li>');
+  if (todo.completed) {
+    newTodo.addClass("done");
+  }
+  $('.list').append(newTodo);
+}
+
+function createTodo() {
+  //sending request to create new todo
+  var usrInput = $('#todoInput').val();
+  $.post('/api/todos', {name: usrInput})
+  .then(function(newTodo) {
+    //clearing out the input field
+    $('#todoInput').val('');
+    addTodo(newTodo);
+  })
+  .catch(function(err) {
+    console.log(err);
+  })
 }
