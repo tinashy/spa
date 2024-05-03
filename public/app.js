4b86cd5749a7d36af4d58d64eb663a0017db2f9b
Nashe Dan <tinashydaniel@gmail.com>
2024-05-03 22:58:04 +0200

    initial ajax call | fetching todos

--- public/app.js
+++ public/app.js
@@ -1,1 +1,14 @@
-alert("aroo");
+$(document).ready(function() {
+  $.getJSON("/api/todos")
+  .then(addTodos)
+  .catch(function(err) {
+    throw Error (err);
+  });
+});
+
+function addTodos(todos) {
+  todos.forEach(function(todo) {
+    var newTodo = $('<li>'+todo.name+'</li>');
+    $('.list').append(newTodo);
+  })
+}
